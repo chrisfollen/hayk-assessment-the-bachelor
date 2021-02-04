9024cd716a50adc95d3e99897fba427c20372494
Chris Follen <christopher.follen@gmail.com>
2021-02-04 13:47:48 -0700

    commit tree db636e94f6db38314cd83bb9017ce9f65e9be41a

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,29 +4,28 @@ require 'json'
 file = File.read('./spec/fixtures/contestants.json')
 data = JSON.parse(file)
 
-def get_contestants(data)
-  contestants = []
-  data.each do |season, people|
-    people.each do |person|
-      contestants.push(person)
-    end
-  end
-  contestants
+# def get_all_contestants(data)
+#   contestants = []
+#   data.each do |season, people|
+#     people.each do |person|
+#       contestants.push(person)
+#     end
+#   end
+# contestants
+# end
+
+def get_all_contestants(data)
+  vals = data.values.flatten
 end
 
 def get_first_name_of_season_winner(data, season)
-  # code here
   contestants = data[season]
-  she_wins = contestants.filter do |person|
-    person["status"] == "Winner"
-  end
+  she_wins = contestants.filter {|person| person["status"] == "Winner"} 
   she_wins[0]["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-  contestants = get_contestants(data)
-  contestants.each do |person|
+  get_all_contestants(data).each do |person|
     if person["occupation"] == occupation
       return person["name"]
     end
@@ -34,29 +33,16 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-  contestants = get_contestants(data)
-  contestants_from_there = contestants.filter do |person|
-    person["hometown"] == hometown
-  end
-  contestants_from_there.length
+  get_all_contestants(data).filter {|person| person["hometown"] == hometown}.length
 end
 
 def get_occupation(data, hometown)
-  # code here
-  contestants = get_contestants(data)
-  contestant = contestants.find do |person|
-    person["hometown"] == hometown
-  end
+  contestant = get_all_contestants(data).find {|person| person["hometown"] == hometown}
   contestant["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
   contestants = data[season]
-  ages = []
-  contestants.each do |person|
-    ages.push(person["age"].to_f)
-  end
+  ages = contestants.map {|person| person["age"].to_f}
   avg_age = (ages.reduce(:+)/ages.length).round
 end
